fd1d41a7eea566cacb9418cbd021dca5caa927d2
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-02-02 21:14:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Implement Atoi/implement-atoi.py
@@ -0,0 +1,30 @@
+#User function template for Python
+
+class Solution:
+    # your task is to complete this function
+    # function should return an integer
+    def atoi(self,s):
+        # Code here
+        # for i in s:
+        #     if i.isalpha():
+        #         return -1
+        # return int(s)
+        for i in range(len(s)):
+            if i!=0 and s[i]=="-":
+                return -1
+            if s[i].isalpha():
+                return -1
+        return int(s)
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        string = input().strip();
+        ob=Solution()
+        print(ob.atoi(string))
+# } Driver Code Ends
