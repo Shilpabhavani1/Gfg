4bd444d04c89284c29d576a710fbe41f48dac501
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-03-26 21:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Additive sequence/additive-sequence.py
@@ -0,0 +1,39 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+# return 1 in case of True and 0 in case of False
+class Solution:
+    def isAdditiveSequence(self, n):
+        #code here
+        def fun(a1,a2,s):
+            if not s:
+                return True
+            a=a1+a2
+            b=str(a)
+            if not s.startswith(b):
+                return False
+            return fun(a2,a,s[len(b):])
+        l=len(n)
+        for i in range(1,l):
+            for j in range(i+1,l):
+                a1,a2=int(n[:i]),int(n[i:j])
+                if fun(a1,a2,n[j:]):
+                    return 1
+        return 0
+        
+
+#{ 
+ # Driver Code Starts.
+        
+if __name__ == "__main__":
+    sol = Solution()
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        print(sol.isAdditiveSequence(s))
+
+# } Driver Code Ends
