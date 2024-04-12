281469c7ea198ce7b92c7799a843bd4c7941991d
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-04-12 20:04:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Products/sum-of-products.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long pairAndSum(int n, long long arr[]) {
+        // code here
+        long long sum=0;
+        for(int i=0;i<32;i++)
+        {
+            long long cnt=0;
+            for(int j=0;j<n;j++)
+            {
+                if (arr[j]&(1<<i)) cnt++;
+            }
+            sum+=(long long)((cnt*(cnt-1))/2)*(1<<i);
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        long long Arr[N];
+        for (int i = 0; i < N; i++) {
+            cin >> Arr[i];
+        }
+        Solution ob;
+        cout << ob.pairAndSum(N, Arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
